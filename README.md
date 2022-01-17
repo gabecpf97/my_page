f0aee36ec5bfe3551aa007c30d7d972786e05515
Gabriel <gabrielcpf2997@gmail.com>
2022-01-17 15:34:02 -0800

    added basic idea on how the page would contain

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# This is the repo for my perosnal website
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to PuiFai Chau page</h1>
+      <a href="https://github.com/gabecpf97/my_page">Github page</a>
+      <h2>Projects: </h2>
+      <ul className="projects">
+        <li className="project">
+          <a href="https://gabecpf97.github.io/clone_book_page/">Clone social media</a>
+        </li>
+      </ul>
     </div>
   );
 }
